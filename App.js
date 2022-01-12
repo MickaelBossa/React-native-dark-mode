47fff985adf573a9a5f618d7abcb78ba0bed20d8
BOSSA Mickael <mickael.bossa@outlook.com>
2022-01-12 11:26:26 +0100

    react native dark mode

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Image } from 'react-native';
 
 export default function App() {
+  // State
+  const [darkMode, setDarkMode] = useState(false);
+
+  // Fonctions
+  const toggleState = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={darkMode ? styles.darkContainer : styles.lightContainer}>
+      {darkMode ? (
+        <Image
+          source={require('./assets/moon.png')}
+          style={{ width: 200, height: 200, marginBottom: 30 }}
+        />
+      ) : (
+        <Image
+          source={require('./assets/sun.png')}
+          style={{ width: 200, height: 200, marginBottom: 30 }}
+        />
+      )}
+      <Button
+        onPress={toggleState}
+        title={darkMode ? 'Passer en mode jour' : 'Passer en mode nuit'}
+      />
+
+      <StatusBar style='auto' />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  lightContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ecf0f1',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  darkContainer: {
+    flex: 1,
+    backgroundColor: '#2c3e50',
     alignItems: 'center',
     justifyContent: 'center',
   },
